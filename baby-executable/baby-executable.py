ba919755adf4907b5eafa99e046250c162a6561c
wj-jason <jason.wang1709@gmail.com>
2023-11-05 00:32:15 -0400

    jason final

--- baby-executable/baby-executable.py
+++ baby-executable/baby-executable.py
@@ -10,15 +10,22 @@ OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
 
 def create_pset(input_pdf, num_mcq, num_long, model):
     
+    # by default, num_mcq = 2
+    # by default, num_long = 3
+    # by default, model = "gpt-3.5-turbo"
+
+    # remove .pdf filename, create output file paths
     input_name = input_pdf[:-4]
     md_out = f"{input_name}_pset.md"
     pdf_out = f"{input_name}_pset.pdf"
 
+    # extract text
     reader = PdfReader(str(input_pdf))
     text = ''
     for page in reader.pages:
         text += page.extract_text()
 
+    # GPT CONFIG
     sys_content = f'''
         You are a problem set writer. Given a set of class notes, compile a problem set based on the relevant content. 
         The output must be in GFM format so it can be easily exported as a PDF file. 
@@ -28,20 +35,18 @@ def create_pset(input_pdf, num_mcq, num_long, model):
         '''
     sys_prompt = [{"role": "system", "content": sys_content}]
     usr_prompt = [{"role": "user", "content": text}]
-
     openai.api_key = OPEN_AI_API_KEY
-
-    print("Starting problem set generation ...")
+    print("Generating problem set ...")
     response = openai.ChatCompletion.create(
         model = str(model),
         messages = sys_prompt + usr_prompt
     )
     print("Problem set generation complete!")
 
+    # PDF conversion and output
     print("Starting PDF conversion ...")
     with open(md_out, "w") as mdfile:
         mdfile.write(response['choices'][0]['message']['content'])
-
     try:
         subprocess.run(["pandoc", "--from=gfm", "--to=pdf", "-o", pdf_out, md_out], check=True)
         print("PDF conversion complete!")
